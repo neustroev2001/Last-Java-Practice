28ae4809992e51ddc92f19b7d88983f2ae6ae0e1
Leonid <neustroev.leonid@mail.ru>
2020-12-20 19:49:35 +0300

    done

---
+++ Practice29-30/src/task29_30/main29_30.java
@@ -0,0 +1,8 @@
+package task29_30;
+
+public class main29_30 {
+    public static void main(String[] args) {
+        WordCounter wordCounter= new WordCounter();
+        wordCounter.counter();
+    }
+}
